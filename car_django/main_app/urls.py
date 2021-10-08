43fc68b4c50590f4960253926f85e4e3c4184614
unknown <nike.yogesh@gmail.com>
2021-10-07 17:41:10 -0700

    Lab 1 completed.CRUD is complete.

--- car_django/main_app/urls.py
+++ car_django/main_app/urls.py
@@ -3,5 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.AllCars.as_view(), name="All Cars"), # <- here we have added the new path
-    path('artists/', views.CarsList.as_view(), name="artist_list"),
+     path('about/', views.About.as_view(), name="about_page"),
+    path('cars/', views.CarsList.as_view(), name="cars_list"),
+    path('about/',views.AllCars.as_view(), name="All Cars"),
+    # Route for create
+    path('cars/new/', views.AddCar_Create.as_view(), name="car_create"),
+    
+    # Our new Route including the pk param
+    path('cars/<int:pk>/', views.CarDetail.as_view(), name="car_detail"),
+    path('cars/<int:pk>/update',views.CarUpdate.as_view(), name="car_update"),
+    # Our new Route including the pk param
+    path('cars/<int:pk>/delete',views.CarDelete.as_view(), name="car_delete"),
 ]
--- car_django/main_app/views.py
+++ car_django/main_app/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+# This will import the class we are extending 
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.views.generic import DetailView
+
+
 from . models import CarManufacturer
 
 # Create your views here.
 class AllCars(TemplateView):
     template_name = "home.html"
-
+    
+class About(TemplateView):
+    template_name = "about_page.html"
 #  #This is constructor
 # class CarManufacturer:
 #     def __init__(self, name, logo, Information):
@@ -15,9 +22,41 @@ class AllCars(TemplateView):
         
 #controller
 class CarsList(TemplateView):
-    template_name = "artist_list.html"
+    template_name = "cars_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["all_Cars"] = CarManufacturer.objects.all() # Here we are using the model to query the database for us.
+        # to get the query parameter we have to acccess it in the request.GET dictionary object        
+        name = self.request.GET.get("name")
+        # If a query exists we will filter by name 
+        if name != None:
+            # .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
+            context["all_Cars"] = CarManufacturer.objects.filter(name__icontains=name)
+            context["header"] = f"Searching for {name}"
+        else:
+            context["all_Cars"] = CarManufacturer.objects.all()
+            context["header"] = "Top USA Car Manufacturer"
         return context
+    
+class AddCar_Create(CreateView):
+    model = CarManufacturer
+    fields = ['name', 'logo', 'information']
+    template_name = "car_create.html"
+    success_url = "/cars/"  #redirect path
+    
+    
+class CarDetail(DetailView):
+    model = CarManufacturer
+    template_name = "car_detail.html"
+    
+class CarUpdate(UpdateView):
+    model = CarManufacturer
+    fields = ['name', 'logo', 'information']
+    template_name = "car_update.html"
+    success_url = "/cars/"  #redirect path
+    
+    
+class CarDelete(DeleteView):
+    model = CarManufacturer
+    template_name = "car_delete.html"
+    success_url = "/cars/" #redirect path
